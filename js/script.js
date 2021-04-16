b885129dbe41934cb352442c5a2a35725631b197
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-04-16 14:29:55 +0100

    Generating quote functionality works

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
+const quotes = [
+  {
+    quote: 'This is quote 1',
+    author: 'Lawrence',
+  },
+  {
+    quote: 'This is quote 2',
+    author: 'Rena',
+  },
+  {
+    quote: 'This is quote 3',
+    author: 'Other',
+  },
+];
+
 window.onload = initialLoad;
+
 function initialLoad() {
-  console.log('Loaded');
-  document.getElementById('text').innerText = 'Quote';
-  document.getElementById('author').innerText = 'Some famous person';
+  generateQuote();
+}
+
+function generateQuote() {
+  let quoteSize = quotes.length;
+  let randomIndex = Math.floor(Math.random() * quoteSize);
+  let randomQuote = quotes[randomIndex];
+
+  document.getElementById('text').innerText = randomQuote.quote;
+  document.getElementById('author').innerText = randomQuote.author;
 }
